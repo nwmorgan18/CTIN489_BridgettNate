661a294db105656a73565c78b442850274802b60
Bridgett O'Grady <bogrady@usc.edu>
2024-03-19 11:05:07 -0700

    some work on the acid colliders

--- Assets/Scripts/IslandIgnore.cs
+++ Assets/Scripts/IslandIgnore.cs
@@ -6,24 +6,27 @@ public class IslandIgnore : MonoBehaviour
 {
     public GameObject swamp;
     private Collider2D swampCollider;
+    static int numBridges = 0;
 
     void Start() {
         swampCollider = swamp.GetComponent<Collider2D>();
+        Debug.Log(gameObject.name);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("PlayerFeet"))
         {
             // Disable the swamp trigger when the player enters the island
-            swampCollider.isTrigger = false;
+            //swampCollider.isTrigger = false;
+            swampCollider.gameObject.SetActive(false);
             Debug.Log("Island");
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("PlayerFeet"))
         {
             // Re-enable the swamp trigger when the player leaves the island
             swampCollider.isTrigger = true;
