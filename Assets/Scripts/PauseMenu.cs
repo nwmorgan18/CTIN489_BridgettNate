2bda10843f226af517036040db75ada761d7634f
Bridgett O'Grady <bogrady@usc.edu>
2024-04-18 10:31:06 -0700

    pause menu updates

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -6,12 +6,37 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject controlScreen;
+    [SerializeField] GameObject restart;
+    [SerializeField] GameObject resume;
+    [SerializeField] GameObject controls;
     public void Pause() {
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
     }
 
     public void Controls() {
-        SceneManager.LoadScene("Controls");
+        restart.SetActive(false);
+        resume.SetActive(false);
+        controls.SetActive(false);
+        controlScreen.SetActive(true);
+    }
+
+    public void Restart() {
+        string currentscene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentscene);
+        Time.timeScale = 1;
+    }
+
+    public void Resume() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Back() {
+        controls.SetActive(true);
+        restart.SetActive(true);
+        resume.SetActive(true);
+        controlScreen.SetActive(false);
     }
 }
